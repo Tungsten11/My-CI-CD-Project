e10284aa88ee676797329c98eb8bb10dbccaebf0
Wendell Segundo <xiamdev1@gmail.com>
2025-07-24 13:42:19 +0800

    testing my app

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -3,10 +3,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "web" {
-    ami = ""
+    ami = "ami-0062e0576ebcaa2a9"
     instance_type = t2.micro
-    key_name = "your-key-pair-name"
-
+    key_name = "my-cicd-app-key"
 
     user_data = <<-EOF
             #!/bin/bash
@@ -21,6 +20,26 @@ resource "aws_instance" "web" {
         Name = "flask-docker-app" 
     }
 }
+
+resource "aws_cloudwatch_log_group" "flask_logs" {
+    name = "/aws/ec2/flaskapp"
+    retention_in_days = 7
+}
+
+resource "aws_s3_bucket" "app_logs" {
+    bucket = "my-flask-app-logs-${random.id.suffix.hex}"
+    
+
+    tags = {
+        Name = "flask-app-logs"
+    }
+}
+
+resource "random_id" "suffix" {
+    byte_length = 4
+}
+
+
 
 
 
