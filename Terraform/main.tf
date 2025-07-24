8f374e72c9230dbea3922568a25ec57b3b23a148
Wendell Segundo <xiamdev1@gmail.com>
2025-07-24 15:16:11 +0800

    updating main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -2,10 +2,44 @@ provider "aws" {
     region = "ap-southeast-1" #region
 }
 
+# Get the default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Create Security Group
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = data.aws_vpc.default.id
+  
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 resource "aws_instance" "web" {
     ami = "ami-0062e0576ebcaa2a9"
     instance_type = "t2.micro"
     key_name = "my-cicd-app-key"
+    vpc_security_group_ids = [aws_security_group.web_sg.id]
 
     user_data = <<-EOF
             #!/bin/bash
